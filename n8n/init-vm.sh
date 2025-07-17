46933ff62860be0e8100d4a1e1f0a44b114fdb31
UsuariooRoot <burnedprogrammer@gmail.com>
2025-07-17 17:52:21 -0500

    fix init-vm.sh

--- n8n/init-vm.sh
+++ n8n/init-vm.sh
@@ -3,15 +3,15 @@
 set -e  # Terminate if there is any error
 
 # Update system
-echo "Update System"
+echo "#### Update System ####"
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Uninstall all conflicting packages for Docker installation
-echo "Uninstall all conflicting packages for Docker installation..."
+echo "#### Uninstall all conflicting packages for Docker installation... ####"
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Set up Docker's apt repository.
-echo "Set up Docker's apt repository"
+echo "#### Set up Docker's apt repository ####"
 ##  Add Docker's official GPG key:
 echo "Add Docker's official GPG key..."
 sudo apt-get install ca-certificates curl
@@ -28,26 +28,23 @@ echo \
 sudo apt-get update
 
 # Install the Docker packages
-echo "Install the Docker packages"
+echo "#### Install the Docker packages ####"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "Create the docker group and add user"
-# Create the docker group
-sudo groupadd docker
-
 # Add user to the `docker` group
+echo "Add user to the `docker` group"
 sudo usermod -aG docker $USER
 
-# Restart VM
-sudo reboot
-
 # Instalar Nginx y Certbot
+echo "Install Nginx and Certbot"
 sudo apt-get install -y nginx certbot python3-certbot-nginx
 
 # Habilitar servicios
+echo "Enabling Nginx server"
 sudo systemctl enable nginx
 
 # Crear carpeta de proyecto
+echo "Create $HOME/app directory"
 mkdir -p /home/azureuser/app
 
 
